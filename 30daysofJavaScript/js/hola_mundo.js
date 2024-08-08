f4e581e140bb9188531154d5a9f8800eda6ce62e
OscarEdSG <103353751+OscarEdSG@users.noreply.github.com>
2024-08-08 01:16:21 -0600

    Add files via upload

---
+++ 30daysofJavaScript/js/hola_mundo.js
@@ -0,0 +1,4 @@
+let edad = 30;
+let cadena = "puto"
+console.log(cadena)
+console.log(edad)
---
+++ 30daysofJavaScript/js/introduction.js
@@ -0,0 +1,8 @@
+let nombre = "Oscar";
+let apellido = "Solis";
+let edad = "24";
+let pais = "México";
+let ciudad = "GDL";
+let estadoCivil = false; 
+
+console.log (nombre, apellido, edad, pais, ciudad, estadoCivil);
